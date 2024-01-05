0a10eaf9b0b15effcc53b6e1ffdfd218c5f3a138
Eric Pan <75194970+EricPanDev@users.noreply.github.com>
2024-01-04 18:37:01 -0800

    Add Disable Telemetry option

--- optimizer.bat
+++ optimizer.bat
@@ -8,7 +8,31 @@ if %errorlevel% neq 0 (
     exit /b 1
 )
 
-:: The rest of your script goes here
+:: Ask if the user wants to disable Windows Telemetry
+echo Do you want to disable Windows Telemetry? (Y/N)
+choice /c yn /m "Type Y for Yes or N for No."
+
+if errorlevel 2 (
+    echo You chose No. Windows Telemetry will not be disabled.
+) else (
+    echo You chose Yes. Disabling Windows Telemetry.
+
+    :: Disable telemetry settings
+    reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v AllowTelemetry /t REG_DWORD /d 0 /f
+    reg add "HKLM\SOFTWARE\Policies\Microsoft\SQMClient\Windows" /v CEIPEnable /t REG_DWORD /d 0 /f
+    reg add "HKLM\SYSTEM\CurrentControlSet\Services\DiagTrack" /v Start /t REG_DWORD /d 4 /f
+    reg add "HKLM\SYSTEM\CurrentControlSet\Services\dmwappushservice" /v Start /t REG_DWORD /d 4 /f
+
+    :: Restart the required services
+    net stop DiagTrack
+    net stop dmwappushservice
+    net start DiagTrack
+    net start dmwappushservice
+    
+    echo Windows Telemetry has been disabled.
+)
+
+:: Ask if the user wants to remove Windows Defender
 echo Do you want to remove Windows Defender? (Y/N)
 choice /c yn /m "Type Y for Yes or N for No."
 
